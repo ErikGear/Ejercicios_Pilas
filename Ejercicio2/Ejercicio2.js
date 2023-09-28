d0a87231a4a3eee3feb6ff9c1ed8815eb1290044
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-28 16:09:27 -0600

    Agrgando comentarios

--- Ejercicio2/Ejercicio2.js
+++ Ejercicio2/Ejercicio2.js
@@ -49,20 +49,30 @@ class Pila {
 //Entradas: Pila, nuevo, viejo
 //salida: nuevaPila, con pop de los elementos mas nuevos y reemplazando la primera coincidencia del valor viejo con el valor nuevo
 function reemplazar(pila, nuevo, viejo) {
+
+  //Declaro un pila temporal, ya que me servira para realizar pusheo de valores nuevos y los actuales, alternando un nuevo y un valor actual
   const tmp = new Pila();
 
+  //voy extrayendo todos los elementos de la pila original
   while (!pila.isEmpty()) {
     
+    //por cada iteracion. almaceno un elemento de la pila original
     const item = pila.pop();
 
+    //comparo el elemento actual con el segundo valor, de parametro de mi función, en caso de ser iguales almaceno el nuevo elemento en la pila temporal TMP
     if (item === viejo) {
       tmp.push(nuevo);
     } else {
+      //caso contrario almaceno como tal el elemento orginal
       tmp.push(item);
     }
+    //en este if es donde se hace el reemplazo en las posiciones 
+    //donde aparece el elemento viejo y se reemplza con el nuevo vaslor
     
   }
-
+  
+  //finalmente ya tengo el reemplazo de valores en la pila temporal TMP, 
+  //vacio la pila TMP y esos valores los agrego en la pila orginal
   while(!tmp.isEmpty()) {
     pila.push(tmp.pop());
   }
@@ -89,6 +99,7 @@ pilaNumbers.toString();
 //aplicando función reemplazar
 reemplazar(pilaNumbers, 8, 2);
 
+//imprimiendo en consola la pila final
 console.log("\n Pila Final");
 pilaNumbers.toString();
 
