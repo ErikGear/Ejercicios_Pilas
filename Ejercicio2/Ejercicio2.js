5663b359bcbdf3790e67d1724916d0133065ff23
ErikGear <erikgalindo23@aragon.unam.mx>
2023-09-28 15:43:40 -0600

    Ejercicio 2 - Pilas

--- Ejercicio2/Ejercicio2.js
+++ Ejercicio2/Ejercicio2.js
@@ -1,70 +1,77 @@
-class Pila{
-    
-    #data = [];
+class Pila {
+  #data = [];
 
-    push(item){
-        this.#data.push(item);
-    }
+  push(item) {
+    this.#data.push(item);
+  }
 
-    
-    pop(){
-      try {
-        if (this.#data.length === 0) {
-            throw "Error, no hay elmentos en la Pila";
-        } else {
-            return this.#data.pop();
-        }
-      } catch (e) {
-        console.log(e);
-      }
-    }
-
-   
-    isEmpty(){
-        return this.#data.length === 0;
-    }
-
-    
-    peek(){
-      if (this.#data !== 0) {
-        let ultimo = this.#data.length;
-        return this.#data[ultimo - 1];
+  pop() {
+    try {
+      if (this.#data.length === 0) {
+        throw "Error, no hay elmentos en la Pila";
       } else {
-        return -1;
+        return this.#data.pop();
       }
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    toString(){
-        let longitud = this.#data.length-1;
-        for (let i = longitud; i >= 0 ; i--) {
-            console.log(`Item: ${this.#data[i]}`);
-        }
-    }
+  isEmpty() {
+    return this.#data.length === 0;
+  }
 
-    getSize(){
-      return this.#data.length;
+  peek() {
+    if (this.#data !== 0) {
+      let ultimo = this.#data.length;
+      return this.#data[ultimo - 1];
+    } else {
+      return -1;
     }
+  }
+
+  toString() {
+    let longitud = this.#data.length - 1;
+    for (let i = longitud; i >= 0; i--) {
+      console.log(`Item: ${this.#data[i]}`);
+    }
+  }
+
+  getSize() {
+    return this.#data.length;
+  }
+
+  clear() {
+    this.#data = [];
+  }
 }
 
 //Entradas: Pila, nuevo, viejo
-//salida: nuevaPila, con pop de los elementos mas nuevos y reemplazando la prmiera coincidencia del valor viejo con el valor nuevo}
-function reemplazar(pila, nuevo, viejo){
-    const pilaOld = new Pila();
-    const pilaNew = new Pila();
-    
-    while (!pila.isEmpty()) {
-        const item = pila.pop();
+//salida: nuevaPila, con pop de los elementos mas nuevos y reemplazando la primera coincidencia del valor viejo con el valor nuevo
+function reemplazar(pila, nuevo, viejo) {
+  const tmp = new Pila();
 
-        if (item === viejo) {
-            
-        } else {
-            
-        }
+  while (!pila.isEmpty()) {
+    
+    const item = pila.pop();
+
+    if (item === viejo) {
+      tmp.push(nuevo);
+    } else {
+      tmp.push(item);
     }
+    
+  }
+
+  while(!tmp.isEmpty()) {
+    pila.push(tmp.pop());
+  }
+
+  
 }
 
 //pila inicial
-const pilaNumbers  = new Pila();
+const pilaNumbers = new Pila();
 pilaNumbers.push(3);
 pilaNumbers.push(2);
 pilaNumbers.push(3);
@@ -76,8 +83,12 @@ pilaNumbers.push(2);
 pilaNumbers.push(5);
 pilaNumbers.push(5);
 
+//imprimiendo pila incial
 pilaNumbers.toString();
 
+//aplicando función reemplazar
+reemplazar(pilaNumbers, 8, 2);
 
-
+console.log("\n Pila Final");
+pilaNumbers.toString();
 
